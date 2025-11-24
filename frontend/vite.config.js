41d9ff1aab386de13addf9822eca86820c99c3e0
Zaid Kamil <kamilzaid53@gmail.com>
2025-11-24 20:54:43 +0530

    Updated backedn route in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000'
+        target: 'https://tuteskillz-production.up.railway.app/',
+        changeOrigin: true,
+        secure: true,
       }
     }
   },
