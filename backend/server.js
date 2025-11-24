79a38cd9433692b6d51003ea444b1d4d76f227ca
Zaid Kamil <kamilzaid53@gmail.com>
2025-11-24 22:07:50 +0530

    Server origin updated

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,16 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tuteskillz.vercel.app"], 
+    origin: [
+      "http://localhost:5173", // for local dev
+      "https://tuteskillz-git-main-sheda3838s-projects.vercel.app",
+      "https://tuteskillz-n7yuc3rxw-sheda3838s-projects.vercel.app"
+    ],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
 );
+
 
 
 app.use(express.json({ limit: '50mb' }));
